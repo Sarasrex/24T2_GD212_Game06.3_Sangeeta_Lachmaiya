885781f9eeb6de388fdedaea80b5cc27c3a415c4
Sangeeta Lachmaiya <1031886@student.sae.edu.au>
2024-03-19 16:24:46 +1100

    Downloaded asset pack and created level environment

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float mouseSensitivity = 500f;
+    public float mouseSensitivity = 1000f;
+    public float jumpForce = 5f;
 
     private Transform playerBody;
     private Transform cameraTransform;
     private float xRotation = 0f;
+    private bool isGrounded; 
 
     void Start()
     {
@@ -24,8 +26,9 @@ public class PlayerMovement : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * moveX + transform.forward * moveZ;
-        playerBody.Translate(move * moveSpeed * Time.deltaTime);
+        //Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        Vector3 move = new Vector3 (moveX, 0, moveZ) * moveSpeed * Time.deltaTime;
+        playerBody.Translate(move);
 
         // Rotation
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
@@ -36,5 +39,27 @@ public class PlayerMovement : MonoBehaviour
 
         cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+
+
+        if (Input.GetKeyDown(KeyCode.Space)&& isGrounded)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up*jumpForce,ForceMode.Impulse);
+            isGrounded = false;
+        }
     }
+
+    void OnCollisionStay(Collision collision)
+    {
+        // Check if the player is grounded
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            if (Vector3.Dot(contact.normal, Vector3.up) > 0.7f)
+            {
+                isGrounded = true;
+            }
+        }
+    }
+
+
+
 }
